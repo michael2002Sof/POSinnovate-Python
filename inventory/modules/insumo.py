b20cbbd19841024d6394bd7122c2daa505a75e38
Brayan <bscrsecundaria@gmail.com>
2025-11-19 10:28:33 -0500

    RF 2.3 - Gestión de Alertas de Stock Bajo (Modulo de Inventario)

--- inventory/modules/insumo.py
+++ inventory/modules/insumo.py
@@ -1,7 +1,7 @@
 insumos = []
 
 #=================================================================================================
-#Clase de Insumos:
+# Clase de Insumos
 #=================================================================================================
 
 class Insumo:
@@ -21,25 +21,24 @@ class Insumo:
         self.fecha_registro = fecha_registro
         self.costo = costo
 
-#=================================================================================================
-#Registro / Modificación de Insumo (RF 2.1)
-#=================================================================================================
-
+#=============================================================================================
+# Registro / Modificación de Insumo (RF 2.1)
+#=============================================================================================
     def registrar_insumos():
         unidades_validas = ["litros", "metros", "unidad", "unidades"]
 
-        while True:  # permite registrar/modificar varios seguidos
+        while True:
 
             nombre_ = input("\nIngresa el nombre del insumo: ").strip().lower()
 
-            #Buscar si ya existe
+            # Buscar si ya existe
             insumo_existente = None
             for insumo in insumos:
                 if insumo.nombre == nombre_:
                     insumo_existente = insumo
                     break
 
-            #CASO 1: INSUMO EXISTENTE
+            # CASO 1: INSUMO EXISTENTE
             if insumo_existente is not None:
                 print(f"\nEl insumo '{nombre_}' ya se encuentra registrado.")
 
@@ -47,7 +46,7 @@ class Insumo:
                     opcion_2 = input("¿Desea modificar sus datos (Cantidad - Costo)? (SI/NO): ").strip().lower()
 
                     if opcion_2 == "si":
-                        #Nueva cantidad
+                        # Nueva cantidad
                         while True:
                             try:
                                 cantidad_nueva = float(input("Ingrese la cantidad a agregar: "))
@@ -56,9 +55,9 @@ class Insumo:
                                 else:
                                     print("La cantidad no puede ser negativa.\n")
                             except ValueError:
-                                print("Cantidad inválida. Debe ser número.\n")
+                                print("Cantidad inválida.\n")
 
-                        #Nuevo costo
+                        # Nuevo costo
                         while True:
                             costo_texto = input("Ingrese el costo a sumar: ").strip()
                             try:
@@ -70,85 +69,82 @@ class Insumo:
                             except ValueError:
                                 print("Costo inválido.\n")
 
-                        #Actualizar insumo existente
+                        # Actualizar insumo
                         insumo_existente.stock_actual += cantidad_nueva
                         insumo_existente.costo += costo_nuevo
 
-                        print("\nInsumo actualizado correctamente.")
+                        print(" ")
+                        print("="*40)
+                        print("Insumo actualizado correctamente.")
                         print(f"Nuevo stock: {insumo_existente.stock_actual} {insumo_existente.unidad_medida}")
-                        print(f"Nuevo costo acumulado: ${insumo_existente.costo}\n")
+                        print(f"Nuevo costo acumulado: ${insumo_existente.costo}")
+                        print("="*40)
+
+                        # RF 2.3
+                        mostrar_resumen_alertas()
                         break
 
                     elif opcion_2 == "no":
                         print("No se modificó el insumo.\n")
                         break
                     else:
-                        print("Opción inválida, responde SI o NO.\n")
+                        print("Opción inválida.\n")
 
-            #CASO 2: NUEVO INSUMO
+            # CASO 2: NUEVO INSUMO
             else:
-                #Unidad de medida
+                # Unidad medida
                 while True:
                     unidad_medida_ = input("Ingrese la unidad de medida (Litros/Metros/Unidad/Unidades): ").strip().lower()
                     if unidad_medida_ in unidades_validas:
                         break
                     else:
-                        print("Unidad no válida, ingresa una unidad permitida.\n")
+                        print("Unidad no válida.\n")
 
-                #Fecha
+                # Fecha
                 while True:
                     fecha_registro_ = input("Ingrese la fecha (DD/MM/AAAA): ").strip()
                     partes = fecha_registro_.split("/")
 
                     if len(partes) == 3:
                         dia, mes, año = partes
-
                         if dia.isdigit() and mes.isdigit() and año.isdigit():
-                            if len(dia) == 2 and len(mes) == 2 and len(año) == 4:
+                            if len(dia)==2 and len(mes)==2 and len(año)==4:
                                 dia = int(dia)
                                 mes = int(mes)
                                 año = int(año)
-
                                 if 1 <= dia <= 31 and 1 <= mes <= 12 and año >= 2025:
                                     break
 
-                    print("Fecha inválida, inténtalo nuevamente.\n")
+                    print("Fecha inválida.\n")
 
-                #Cantidad
+                # Stock inicial
                 while True:
                     try:
                         stock_actual_ = float(input("Stock inicial: "))
                         if stock_actual_ >= 0:
                             break
-                        else:
-                            print("La cantidad no puede ser negativa.\n")
                     except ValueError:
-                        print("Debe ser número, inténtalo nuevamente.\n")
+                        print("Cantidad inválida.\n")
 
-                #Stock mínimo
+                # Stock mínimo
                 while True:
                     try:
                         stock_minimo_ = float(input("Stock mínimo: "))
                         if stock_minimo_ >= 0:
                             break
-                        else:
-                            print("No puede ser negativo.\n")
                     except ValueError:
-                        print("Debe ser número.\n")
+                        print("Cantidad inválida.\n")
 
-                #Costo inicial
+                # Costo inicial
                 while True:
-                    costo_texto = input("Costo inicial: ").strip()
                     try:
-                        costo_ = float(costo_texto)
+                        costo_ = float(input("Costo inicial: "))
                         if costo_ >= 0:
                             break
-                        else:
-                            print("Costo no puede ser negativo.\n")
                     except ValueError:
-                        print("Debe ser número, inténtalo nuevamente.\n")
+                        print("Costo inválido.\n")
 
-                #Crear insumo nuevo
+                # Crear insumo
                 insumo = Insumo(
                     nombre_,
                     unidad_medida_,
@@ -159,35 +155,51 @@ class Insumo:
                 )
 
                 insumos.append(insumo)
-                print("\nInsumo registrado correctamente.\n")
 
-            #Pregunta final para seguir o volver al menú
+                print(" ")
+                print("="*40)
+                print("Insumo registrado correctamente.")
+                print("="*40)
+
+                # RF 2.3
+                mostrar_resumen_alertas()
+
+            # Continuar o salir
             continuar = input("¿Deseas registrar otro insumo? (SI/NO): ").strip().lower()
             if continuar != "si":
                 print("\nSaliendo del registro de insumos...\n")
                 break
 
-#=================================================================================================
-#Consulta de insumos (RF 2.2)
-#=================================================================================================
-
+#=============================================================================================
+# Consulta de Insumos (RF 2.2)
+#=============================================================================================
     def consultar_insumos():
         if not insumos:
-            print("\nNo hay insumos registrados en el inventario.\n")
+
+            print("="*40)
+            print("\nNo hay insumos registrados.\n")
+            print("="*40)
+
             return
 
         while True:
-            print("\n=== CONSULTA DE INSUMOS ===")
+
+            print(" ")
+            print("="*40)
+            print("Consulta de Insumos")
+            print("="*40)
+
             print("1. Buscar por código")
             print("2. Buscar por nombre")
-            print("3. Ver todos los insumos")
+            print("3. Ver todos")
             print("4. Volver\n")
 
             opcion = input("Selecciona una opción: ").strip()
 
+            # Buscar por código
             if opcion == "1":
                 try:
-                    codigo_buscar = int(input("Ingresa el código del insumo: ").strip())
+                    codigo_buscar = int(input("Ingresa el código: ").strip())
                 except ValueError:
                     print("Código inválido.\n")
                     continue
@@ -195,7 +207,6 @@ class Insumo:
                 encontrado = False
                 for insumo in insumos:
                     if insumo.codigo == codigo_buscar:
-                        print("\nInsumo encontrado:")
                         print(insumo)
                         encontrado = True
                         break
@@ -203,46 +214,96 @@ class Insumo:
                 if not encontrado:
                     print("\nNo se encontró un insumo con ese código.\n")
 
+            # Buscar por nombre
             elif opcion == "2":
-                nombre_buscar = input("Ingresa el nombre del insumo: ").strip().lower()
+                nombre_buscar = input("Nombre: ").strip().lower()
                 encontrados = [i for i in insumos if nombre_buscar in i.nombre]
 
                 if encontrados:
-                    print("\nInsumos encontrados:")
                     for insumo in encontrados:
                         print(insumo)
                 else:
-                    print("\nNo se encontraron insumos con ese nombre.\n")
+                    print("\nNo se encontraron insumos.\n")
 
+            # Ver todos
             elif opcion == "3":
-                print("\nListado completo de insumos:")
                 for insumo in insumos:
                     print(insumo)
 
             elif opcion == "4":
-                print("\nVolviendo al menú principal...\n")
                 break
 
             else:
                 print("Opción no válida.\n")
 
+
     def __str__(self):
         return (
-            f"\nFecha registro: {self.fecha_registro}\n"
-            f"Codigo: {self.codigo} | Nombre: {self.nombre} | "
+            f"\nFecha: {self.fecha_registro}\n"
+            f"Código: {self.codigo} | Nombre: {self.nombre} | "
             f"Cantidad: {self.stock_actual} {self.unidad_medida} | "
             f"Costo acumulado: ${self.costo}"
         )
-    
+
+#=================================================================================================
+# Alertas por Bajo Stock (RF 2.3)
 #=================================================================================================
 
-#Menú provisional
+def insumo_en_alerta(insumo):
+    return insumo.stock_actual <= insumo.stock_minimo
+
+def obtener_insumos_en_alerta():
+    return [i for i in insumos if insumo_en_alerta(i)]
+
+def mostrar_alertas_stock():
+    alertas = obtener_insumos_en_alerta()
+
+    if not alertas:
+        print("\nNo hay insumos con stock bajo.\n")
+        return
+
+    print(" ")
+    print("="*40)
+    print("Alertas de Stock Bajo")
+    print("="*40)
+
+    for insumo in alertas:
+
+        print(f"\n[ALERTA] Código: {insumo.codigo} | Nombre: {insumo.nombre}")
+        print(f"Stock actual: {insumo.stock_actual} {insumo.unidad_medida}")
+        print(f"Stock mínimo: {insumo.stock_minimo}")
+
+        if insumo.stock_actual < insumo.stock_minimo:
+            deficit = insumo.stock_minimo - insumo.stock_actual
+            print(f"Faltan {deficit} {insumo.unidad_medida}.\n")
+
+def mostrar_resumen_alertas():
+    alertas = obtener_insumos_en_alerta()
+    cantidad = len(alertas)
+
+    if cantidad == 0:
+        print("\nNo hay alertas de stock.\n")
+    else:
+        print(f"\nHay {cantidad} insumo(s) con stock bajo.")
+        print("Revisa el menú 'Ver alertas de stock bajo'.\n")
+
+
+#=================================================================================================
+# Menú Provicional
+#=================================================================================================
+
 def menu_inventario():
     while True:
-        print("\n===== MÓDULO DE INVENTARIO =====")
+        
+        print("")
+        print("="*40)
+        print("Modulo de Inventario")
+        print("="*40)
+
         print("1. Registrar / Modificar Insumo.")
         print("2. Consultar Insumos.")
-        print("3. Salir\n")
+        print("3. Ver alertas de stock bajo.")
+        print("4. Salir\n")
 
         opcion = input("Selecciona una opción: ").strip()
 
@@ -253,7 +314,10 @@ def menu_inventario():
             Insumo.consultar_insumos()
 
         elif opcion == "3":
-            print("\nSaliendo del módulo de inventario...\n")
+            mostrar_alertas_stock()
+
+        elif opcion == "4":
+            print("\nSaliendo. del Sistema...\n")
             break
 
         else:
